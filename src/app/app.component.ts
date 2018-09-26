927deccd15fdc9700303e460a1b777e99937ad37
jullyanehawkins <jullyanehawkins@gmail.com>
2018-09-26 11:38:44 -0600

    Test with title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RSI ABQ Meme Generator';
+  title = 'RSI ABQ Meme Generatr';
 }
