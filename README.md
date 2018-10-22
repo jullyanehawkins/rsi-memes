92f21633b7fa057df97831ee3afc9feba39ca44c
mauriiim <mmonsivais@unm.edu>
2018-10-22 11:49:37 -0600

    -Updated README to include running instructions -removed unused imports from main file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # RSI Memes Server
 
-## Requirements
-* Maven 
+## Executing
+* Maven 3.5.4 needed to run code
+* or without having Maven installed, Maven compiled jar can be executed with: `mvnw spring-boot:run `
--- src/main/java/com/rsi/memegenerator/MemeGeneratorApplication.java
+++ src/main/java/com/rsi/memegenerator/MemeGeneratorApplication.java
@@ -1,17 +1,14 @@
 package com.rsi.memegenerator;
 
-//import com.rsi.memegenerator.model.User;
-//import com.rsi.memegenerator.repository.UserRepository;
-
 import com.rsi.memegenerator.model.User;
 import com.rsi.memegenerator.repository.UserRepository;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.stream.Stream;
 
