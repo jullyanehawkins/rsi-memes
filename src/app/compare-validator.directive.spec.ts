a02e1983be8f18cdbf2fec9f7e6c139f35ce7f98
Reyna Cardoza <reyna.cardoza@ruralsource.com>
2018-11-07 12:19:58 -0700

    deleted compare validator file

--- src/app/compare-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CompareValidatorDirective } from './compare-validator.directive';
-
-describe('CompareValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CompareValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/image-result/image-result.component.ts
+++ src/app/image-result/image-result.component.ts
@@ -45,13 +45,13 @@ export class ImageResultComponent implements OnInit {
   }
   onSelectFile(event) {
     if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
+      let reader = new FileReader();
 
       reader.readAsDataURL(event.target.files[0]);
 
       reader.onload = event => {
         this.url = event.target.result;
-      }
+      };
       // onUpload() {
 
       // }
