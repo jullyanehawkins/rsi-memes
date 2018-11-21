ffb783430e74de33f4b66114392fcd517363a7ff
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-11-21 07:54:18 -0700

    images are getting captioned now

--- src/app/image-result/image-result.component.ts
+++ src/app/image-result/image-result.component.ts
@@ -20,27 +20,31 @@ export class ImageResultComponent implements OnInit {
   topCaptions: string;
   bottomCaptions: string;
   canvas: any;
+  origImage: any;
   constructor(private imageService: ImageService, private storageService: StorageService) { }
 
   context: CanvasRenderingContext2D;
   @ViewChild('imgCanvas') imgCanvas;
 
   onSelectImage(e: any): void {
-    let canvas = this.imgCanvas.nativeElement;
-    let context = canvas.getContext('2d');
+    const canvas = this.imgCanvas.nativeElement;
+    const context = canvas.getContext('2d');
     context.clearRect(200, 200, 350, 350);
-
+    const _this = this;
     // show rendered image to canvas
-    let render = new FileReader();
-    render.onload = function(event) {
-      let img = new Image();
-      img.onload = function() {
+    const render = new FileReader();
+    render.onload = function (event) {
+      const img = new Image();
+
+      img.onload = function () {
+        console.log('ONLOAD');
         canvas.width = img.width;
         canvas.height = img.height;
         context.drawImage(img, 0, 0);
-
+        _this.origImage = img;
       };
       img.src = event.target.result;
+
       console.log(e.target.files[0]);
     };
     render.readAsDataURL(e.target.files[0]);
@@ -50,15 +54,37 @@ export class ImageResultComponent implements OnInit {
   }
   updateCaptions(e) {
     const context = this.canvas.getContext('2d');
-  context.lineWidth = 4;
-  context.font = '110pt sans-serif';
-  context.strokeStyle = 'black';
-  context.fillStyle = 'white';
-  context.textAlign = 'left';
-  context.textBaseline = 'top';
-context.strokeText(this.topCaptions, 0 , 1);
-context.strokeText(this.bottomCaptions, 0, 1);
-// context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height); // clearing canvas
+    context.drawImage(this.origImage, 0, 0); // drawing originalImage on canvas
+
+    const imageCenterX = this.canvas.width / 2; // image center
+    context.strokeStyle = 'black'; // stroke color
+    context.fillStyle = 'white'; // text color
+    context.textAlign = 'center'; // draw text centered
+    context.textBaseline = 'top'; // allign text with the top of coordinates
+
+
+    if (this.topCaptions) { // draw only if defined and string length > 0
+
+      // crazy formula to resize fontSize to fit text on image
+      const fontSizeTop = Math.floor(this.canvas.height / (6 + 1.8 * Math.floor(this.topCaptions.length / 4)));
+      context.font = `bold ${fontSizeTop}pt sans-serif`; // set font style for top drawing
+      context.lineWidth = fontSizeTop / 6; // text stroke line width
+      const textYTopOffset = 0.2 * fontSizeTop; // offset space from top
+      context.strokeText(this.topCaptions, imageCenterX, textYTopOffset); // draw only the outline stroke text (black)
+      context.fillText(this.topCaptions, imageCenterX, textYTopOffset); // then draw the filled text (white)
+    }
+
+
+    if (this.bottomCaptions) { // draw only if defined and string length > 0
+      // same crazy formula
+      const fontSizeBottom = Math.floor(this.canvas.height / (6 + 1.8 * Math.floor(this.bottomCaptions.length / 4)));
+      context.font = `bold ${fontSizeBottom}pt sans-serif`;
+      context.lineWidth = fontSizeBottom / 6;
+      const textBottomYOffset = this.canvas.height - 1.4 * fontSizeBottom;
+      context.strokeText(this.bottomCaptions, imageCenterX, textBottomYOffset);
+      context.fillText(this.bottomCaptions, imageCenterX, textBottomYOffset);
+    }
   }
   onUpload() {
     console.log(this.tags);
@@ -96,5 +122,4 @@ context.strokeText(this.bottomCaptions, 0, 1);
   }
   ngOnInit() { }
 }
-
 
