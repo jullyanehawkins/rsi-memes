56aff21731b0906cd64bf5294386fda70a16eb8e
mauriiim <mmonsivais@unm.edu>
2018-11-28 16:57:06 -0700

    --will now route to caption page when user clicks on search result image --removed unused variables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { StorageService } from '../services/storage.service';
 import { Router } from '@angular/router';
 import { ImageHolderService } from '../image-holder.service';
+import { Images } from '../image';
 
 @Component({
   selector: 'app-home',
@@ -9,52 +10,48 @@ import { ImageHolderService } from '../image-holder.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('imgCanvas') imgCanvas;
   searchQuery: '';
   images: any[];
   imageSelected = false;
-  imagesFound = false;
-  searching = false;
   url = '';
   file;
-  selectedImage = null;
   tags: string;
-  topCaptions: string;
-  bottomCaptions: string;
   canvas: any;
   origImage: HTMLImageElement;
 
-   constructor(private storageService: StorageService,
+  constructor(private storageService: StorageService,
     private imageHolder: ImageHolderService,
     private router: Router) { }
 
-   context: CanvasRenderingContext2D;
-   @ViewChild('imgCanvas') imgCanvas;
+  onImageClicked(image: Images) {
+    console.log(image);
+    this.imageHolder.keepImage(image.url);
+    this.router.navigate(['/captions']);
+  }
 
-   onImageSelected(e: any): void {
+  onImageSelected(e: any): void {
     if (e.target.files.length > 0) {
       const canvas = this.imgCanvas.nativeElement;
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    const _this = this;
-    // show rendered image to canvas
-    const render = new FileReader();
-    render.onload = function(event) {
-      const img = new Image();
+      const context = canvas.getContext('2d');
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      const _this = this;
+      // show rendered image to canvas
+      const render = new FileReader();
+      render.onload = function(event) {
 
-      img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0);
-        _this.origImage = img;
-        _this.imageSelected = true;
+        const img = new Image();
+        img.onload = function() {
+          this.imageSelected = true;
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          _this.origImage = img;
+        };
+        img.src = event.target.result;
       };
-      img.src = event.target.result;
-
-    };
-    render.readAsDataURL(e.target.files[0]);
-    this.file = e.target.files[0];
-    this.canvas = canvas;
-    // this.router.navigate(['/captions']);
+      render.readAsDataURL(e.target.files[0]);
+      this.file = e.target.files[0];
     }
   }
 
@@ -65,12 +62,11 @@ export class HomeComponent implements OnInit {
           query,
           urls => this.handleSuccess(urls),
           null,
-          () => (this.searching = false));
+          null);
     }
   }
 
   handleSuccess(response) {
-    this.imagesFound = true;
     this.images = response.map(image => {
       return {
         id: image.id,
@@ -87,10 +83,12 @@ export class HomeComponent implements OnInit {
       this.storageService.uploadMeme(this.file, this.tags + ' meme',
         null,
         (err) => { console.log(err); },
-        () => { this.router.navigate(['/captions']); });
+        () => {
+          this.imageHolder.keepImage(this.canvas.toDataURL());
+          this.router.navigate(['/captions']);
+        });
     }
-    this.imageHolder.keepImage(this.canvas.toDataURL());
-    this.router.navigate(['/captions']);
   }
-   ngOnInit() {}
+
+  ngOnInit() { }
 }
