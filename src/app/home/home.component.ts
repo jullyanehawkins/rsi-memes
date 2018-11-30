4005a47b88e4b2347207bac77b3a6dc7384e2014
mauriiim <mmonsivais@unm.edu>
2018-11-30 15:42:27 -0700

    -- made the logo act the same as home button -- removed gap between search bar and choose file -- also added "or" between both fields

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { StorageService } from '../services/storage.service';
 import { Router } from '@angular/router';
 import { ImageHolderService } from '../image-holder.service';
@@ -10,47 +10,58 @@ import { Images } from '../image';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('imgCanvas') imgCanvas;
-  searchQuery: '';
-  images: any[];
-  imageSelected = false;
-  url = '';
-  file;
-  tags: string;
-  canvas: any;
-  origImage: HTMLImageElement;
+  private searchQuery: '';
+  private images: any[];
+  private url = '';
+  private imageSelected = false;
+  private selectedImg;
+  private file;
+  private tags: string;
+  private canvas: any;
+  private imgCanvas: ElementRef;
+  @ViewChild('imgCanvas') set imageCanvas(imgCanvas: ElementRef) {
+    if (imgCanvas) {
+      this.imgCanvas = imgCanvas;
+      this.setupCanvas();
+    }
+  }
 
   constructor(private storageService: StorageService,
     private imageHolder: ImageHolderService,
     private router: Router) { }
 
+  handleSuccess(response) {
+    this.images = response.map(image => {
+      return {
+        // id: image.id,
+        // title: image.title,
+        url: image,
+      };
+    });
+  }
+
   onImageClicked(image: Images) {
-    console.log(image);
     this.imageHolder.keepImage(image.url);
     this.router.navigate(['/captions']);
   }
 
   onImageSelected(e: any): void {
-    if (e.target.files.length > 0) {
-      this.canvas = this.imgCanvas.nativeElement;
-      const context = this.canvas.getContext('2d');
-      context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      const _this = this;
-      // show rendered image to canvas
-      const render = new FileReader();
-      render.onload = function(event) {
-        const img = new Image();
-        img.onload = function() {
-          _this.imageSelected = true;
-          _this.canvas.width = img.width;
-          _this.canvas.height = img.height;
-          context.drawImage(img, 0, 0);
-          _this.origImage = img;
-        };
-        img.src = event.target.result;
-      };
-      render.readAsDataURL(e.target.files[0]);
-      this.file = e.target.files[0];
+    if (e.target.files.length === 1) {
+      this.selectedImg = e.target.files[0];
+      if (this.imageSelected) { this.setupCanvas(); }
+      this.imageSelected = true;
+    }
+  }
+
+  onUpload() {
+    if (this.file && this.tags) {
+      this.storageService.uploadMeme(this.file, this.tags + ' meme',
+        null,
+        (err) => { console.log(err); },
+        () => {
+          this.imageHolder.keepImage(this.canvas.toDataURL());
+          this.router.navigate(['/captions']);
+        });
     }
   }
 
@@ -65,27 +76,24 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  handleSuccess(response) {
-    this.images = response.map(image => {
-      return {
-        id: image.id,
-        // embed_url: image.embed_url,
-        // title: image.title,
-        url: image,
-        // downsized_url: image.images.downsized.url
+  setupCanvas() {
+    this.canvas = this.imgCanvas.nativeElement;
+    const context = this.canvas.getContext('2d');
+    const _this = this;
+    // show rendered image to canvas
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const img = new Image();
+      img.onload = function() {
+        context.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+        _this.canvas.width = img.width;
+        _this.canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+        _this.selectedImg = img;
       };
-    });
-  }
-  onUpload() {
-    if (this.file && this.tags) {
-      this.storageService.uploadMeme(this.file, this.tags + ' meme',
-        null,
-        (err) => { console.log(err); },
-        () => {
-          this.imageHolder.keepImage(this.canvas.toDataURL());
-          this.router.navigate(['/captions']);
-        });
-    }
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(this.selectedImg);
   }
 
   ngOnInit() { }
