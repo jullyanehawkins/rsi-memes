15fa64bfe18524b0074139e02b8845236247b9d7
Reyna Cardoza <reyna.cardoza@ruralsource.com>
2018-12-04 08:26:07 -0700

    fixed some stuff on the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,97 +4,110 @@ import { Router } from '@angular/router';
 import { ImageHolderService } from '../image-holder.service';
 import { Images } from '../image';
 
+
 @Component({
-selector: 'app-home',
-templateUrl: './home.component.html',
-styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-private searchQuery: '';
-private images: any[];
-private url = '';
-private imageSelected = false;
-private selectedImg;
-private file;
-private tags: string;
-private canvas: any;
-private imgCanvas: ElementRef;
-@ViewChild('imgCanvas') set imageCanvas(imgCanvas: ElementRef) {
-if (imgCanvas) {
-this.imgCanvas = imgCanvas;
-this.setupCanvas();
-}
-}
 
-constructor(private storageService: StorageService,
-private imageHolder: ImageHolderService,
-private router: Router) { }
+  export class HomeComponent implements OnInit {
+    private searchQuery: '';
+    private images: any[];
+    private url = '';
+    private imageSelected = false;
+    private selectedImg;
+    private file;
+    private tags: string;
+    private canvas: any;
+    private imgCanvas: ElementRef;
+    @ViewChild('imgCanvas') set imageCanvas(imgCanvas: ElementRef) {
+    if (imgCanvas) {
+    this.imgCanvas = imgCanvas;
+    this.setupCanvas();
+    }
+    }
+  constructor(
+    private storageService: StorageService,
+    private imageHolder: ImageHolderService,
+    private router: Router
+  ) {}
 
-handleSuccess(response) {
-this.images = response.map(image => {
-return {
-// id: image.id,
-// title: image.title,
-url: image,
-};
-});
-}
+  context: CanvasRenderingContext2D;
+  @ViewChild('imgCanvas') imgCanvas;
 
-onImageClicked(image: Images) {
-this.imageHolder.keepImage(image.url);
-this.router.navigate(['/captions']);
-}
+  onImageSelected(e: any): void {
+    if (e.target.files.length === 1) {
+    this.selectedImg = e.target.files[0];
+    if (this.imageSelected) { this.setupCanvas(); }
+    this.imageSelected = true;
+    }
+    this.imageHolder.keepImage(this.canvas.toDataURL());
+          this.router.navigate(['/captions']);
+  }
+  imageClicked(e: any) {
+    this.imageHolder.keepImage(e.target.src);
+    this.router.navigate(['/captions']);
+  }
 
-onImageSelected(e: any): void {
-if (e.target.files.length === 1) {
-this.selectedImg = e.target.files[0];
-if (this.imageSelected) { this.setupCanvas(); }
-this.imageSelected = true;
-}
-}
+  searchDatabase(query: string) {
+    if (query) {
+      return this.storageService.pullMemes(
+        query,
+        urls => this.handleSuccess(urls),
+        null,
+        () => (this.searching = false)
+      );
+    }
+  }
 
-onUpload() {
-if (this.file && this.tags) {
-this.storageService.uploadMeme(this.file, this.tags + ' meme',
-null,
-(err) => { console.log(err); },
-() => {});
-}
-this.imageHolder.keepImage(this.canvas.toDataURL());
-this.router.navigate(['/captions']);
-}
+  handleSuccess(response) {
+    this.images = response.map(image => {
+    return {
+    // id: image.id,
+    // title: image.title,
+    url: image,
+    };
+    });
+  }
+  onUpload() {
+    if (this.file && this.tags) {
+      this.storageService.uploadMeme(
+        this.file,
+        this.tags + ' meme',
+        null,
+        err => {
+          console.log(err);
+        },
+        () => {
+          this.router.navigate(['/captions']);
+        }
+      );
+    }
+    this.imageHolder.keepImage(this.canvas.toDataURL());
+    this.router.navigate(['/captions']);
+  }
 
-searchDatabase(query: string) {
-if (query) {
-return this.storageService
-.pullMemes(
-query,
-urls => this.handleSuccess(urls),
-null,
-null);
-}
-}
+  setupCanvas() {
+    this.canvas = this.imgCanvas.nativeElement;
+    const context = this.canvas.getContext('2d');
+    const _this = this;
+    // show rendered image to canvas
+    const reader = new FileReader();
+    reader.onload = function(event) {
+    const img = new Image();
+    img.onload = function() {
+    context.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+    _this.canvas.width = img.width;
+    _this.canvas.height = img.height;
+    context.drawImage(img, 0, 0);
+    _this.selectedImg = img;
+    };
+    img.src = event.target.result;
+    console.log(img.src);
+    };
+    reader.readAsDataURL(this.selectedImg);
+    }
 
-setupCanvas() {
-this.canvas = this.imgCanvas.nativeElement;
-const context = this.canvas.getContext('2d');
-const _this = this;
-// show rendered image to canvas
-const reader = new FileReader();
-reader.onload = function(event) {
-const img = new Image();
-img.onload = function() {
-context.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
-_this.canvas.width = img.width;
-_this.canvas.height = img.height;
-context.drawImage(img, 0, 0);
-_this.selectedImg = img;
-};
-img.src = event.target.result;
-console.log(img.src);
-};
-reader.readAsDataURL(this.selectedImg);
-}
-
-ngOnInit() { }
+  ngOnInit() {}
 }
